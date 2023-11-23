85eda29d831d66146a4390fb9c5ab5cfc10a7c75
bunnana <na.nuttanont@gmail.com>
2023-11-23 11:21:40 +0700

    refactor: :zap: add secret dolder

--- src/config/service-accounts.ts
+++ src/config/service-accounts.ts
@@ -1,10 +1,10 @@
+import { AppConfig } from '@src/secrets'
 import { cert, initializeApp } from 'firebase-admin/app'
 import { getAuth } from 'firebase-admin/auth'
 import { getFirestore } from 'firebase-admin/firestore'
-import Appconfig from './service-account.json'
 
 const adminApp = initializeApp({
-  credential: cert(Appconfig as Record<string, string>),
+  credential: cert(AppConfig as Record<string, string>),
 })
 
 export const db = getFirestore(adminApp)
---
+++ src/secrets/index.ts
@@ -0,0 +1,3 @@
+import AppConfig from './service-account.json'
+
+export { AppConfig }
