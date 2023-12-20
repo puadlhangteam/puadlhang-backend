6fa76ae5495628cdf851f8bf4bcedf6f9cae5ec5
bunnana <na.nuttanont@gmail.com>
2023-12-20 23:14:52 +0700

    fix: :ambulance: fix cors origin required

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,17 +1,10 @@
 import { CorsOptions } from 'cors'
 import { Forbidden403Error } from '../utils/CustomError'
 
-const allowOrigins = [
-  'http://localhost',
-  'http://localhost:5173',
-  'http://localhost:5174',
-  'http://localhost:5175',
-  'http://52.63.27.28',
-  'https://puadlhang.cleverse.academy',
-]
+const allowOrigins = ['http://localhost', 'http://52.63.27.28', 'https://puadlhang.cleverse.academy']
 export const corsOptions: CorsOptions = {
   origin: (origin, callback) => {
-    if (!origin || allowOrigins.indexOf(origin) !== -1) {
+    if (origin && allowOrigins.indexOf(origin) !== -1) {
       callback(null, true)
     } else {
       callback(new Forbidden403Error('Not Allowed By CORS'))
